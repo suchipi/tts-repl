659dfc1a723da3b8effb2d961c28957f2e21ec9d
Lily Skye <me@suchipi.com>
2023-12-12 15:53:36 -0700

    avoid aws call if line is unchanged

--- repl.ts
+++ repl.ts
@@ -3,20 +3,29 @@
 
 cd(__dirname);
 
+let last = "";
+
 function say(phrase: string) {
-  $([
-    "aws",
-    "polly",
-    "synthesize-speech",
-    "--output-format",
-    "ogg_vorbis",
-    "--voice-id",
-    "Ivy",
-    "--text",
-    phrase,
-    "out.ogg",
-  ]);
+  if (!phrase.trim()) {
+    return;
+  }
+
+  if (phrase !== last) {
+    $([
+      "aws",
+      "polly",
+      "synthesize-speech",
+      "--output-format",
+      "ogg_vorbis",
+      "--voice-id",
+      "Ivy",
+      "--text",
+      phrase,
+      "out.ogg",
+    ]);
+  }
   $(["ffplay", "-autoexit", "-nodisp", "out.ogg"]);
+  last = phrase;
 }
 
 new InteractivePrompt((msg) => {
